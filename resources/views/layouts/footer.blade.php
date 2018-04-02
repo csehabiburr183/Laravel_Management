dc3598badb08706112796d8f1771d4c8bdef2bb1
csehabiburr183 <csehabiburr183@gmail.com>
2018-04-03 01:16:08 +0600

    Divide Footer Section

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,18 @@
+<footer style="background:black">
+    <p class="text-center" style="color: #ffff00; padding: 10px 2px">
+        Copyright &copy; 2018 By Authority. All Right Reserved. </p>
+</footer>
+<!-- jQuery -->
+<script src="{{asset('admin/js/jquery.min.js')}}"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="{{asset('admin/js/bootstrap.min.js')}}"></script>
+
+<!-- Metis Menu Plugin JavaScript -->
+<script src="{{asset('admin/js/metisMenu.min.js')}}"></script>
+
+<!-- Custom Theme JavaScript -->
+<script src="{{asset('admin/js/startmin.js')}}"></script>
+
+</body>
+</html>
